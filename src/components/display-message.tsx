a059fac5b14d8e7bff15c6db2e7d7aefa639634e
samuel_ndjouli <samuelndj.997@gmail.com>
2024-12-12 12:18:11 +0100

    added a new variable infoMessage

--- src/components/display-message.tsx
+++ src/components/display-message.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { Info } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 
 type Props = {
     co2: number | null;
@@ -9,6 +11,7 @@ type Props = {
 export default function DisplayMessage({ co2, temperature, humidity }: Props) {
     let message = "";
     let backgroundColor = "";
+    let infoMessage = "";
 
     const co2Status = co2
     ? co2 > 1200
@@ -36,18 +39,21 @@ export default function DisplayMessage({ co2, temperature, humidity }: Props) {
 
     // Calculer la priorité (Rouge > Orange > Vert)
     if ([co2Status, tempStatus, humidityStatus].includes("danger")) {
-        backgroundColor = "bg-red-500";
+        backgroundColor = "bg-red-200 text-red-500";
         message = "Danger détecté : Prenez immédiatement des mesures.";
         if (co2 && co2 > 1200) {
         message = "Niveau critique de CO2 détecté. Aérez immédiatement la pièce.";
+        infoMessage = "Le dioxyde de carbone (CO2) est un gaz incolore et inodore. Une concentration élevée peut entraîner des maux de tête, des vertiges, des nausées et des vomissements.";
         } else if (temperature && (temperature < 18 || temperature > 30)) {
         message = "Température critique détectée. Ajustez les conditions.";
+        infoMessage = "La température ambiante idéale se situe entre 18 et 30 degrés Celsius.";
         } else if (humidity && (humidity < 30 || humidity > 70)) {
         message = "Humidité critique détectée. Vérifiez la ventilation.";
+        infoMessage = "Le taux d'humidité idéal se situe entre 30 et 70%. Un taux trop bas peut entraîner une irritation des yeux et des voies respiratoires, tandis qu'un taux trop élevé peut favoriser la croissance de moisissures.";
         }
     } 
     else if ([co2Status, tempStatus, humidityStatus].includes("warning")) {
-        backgroundColor = "bg-orange-400";
+        backgroundColor = "bg-orange-200 text-orange-500";
         message = "Attention : Surveillez les conditions.";
         if (co2 && co2 >= 801 && co2 <= 1200) {
         message = "Concentration élevée de CO2 détectée. Aérez la pièce.";
@@ -58,7 +64,7 @@ export default function DisplayMessage({ co2, temperature, humidity }: Props) {
         }
     } 
     else if (co2Status === "normal" && tempStatus === "normal" && humidityStatus === "normal") {
-        backgroundColor = "bg-green-400";
+        backgroundColor = "bg-green-200 text-green-500";
         message = "Tout va bien. Conditions optimales détectées.";
     } 
     else {
@@ -67,8 +73,16 @@ export default function DisplayMessage({ co2, temperature, humidity }: Props) {
     }
 
   return (
-    <div className={`p-4 rounded text-white ${backgroundColor}`}>
-      <p className="font-semibold">{message}</p>
+    <div className={`flex justify-between p-4 rounded text-white ${backgroundColor}`}>
+        <p className="font-semibold">{message}</p>
+        <Popover>
+            <PopoverTrigger>
+                <Info className="w-5 h-5" />
+            </PopoverTrigger>
+            <PopoverContent>
+                <p className="text-sm">{infoMessage}</p>
+            </PopoverContent>
+        </Popover>
     </div>
   )
 }
